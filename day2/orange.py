0d8858f3165624a28d20f42bd950f107cf529564
Kyle Jorgensen <quintillion977@gmail.com>
2018-05-22 08:28:22 -0700

    ORANGE added.

---
+++ day2/orange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#This will print the word ORANGE using six prints instead of only oneself.
+#Written by: Kyle Jorgensen
+
+print('O', end='')
+print('R', end='')
+print('A', end='')
+print('N', end='')
+print('G', end='')
+print('E', end='\n')
