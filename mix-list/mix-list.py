3826a67dce916f8261ab0b3ac0b3b4c4d3a5e0fb
Kyle Jorgensen <quintillion977@gmail.com>
2018-05-22 09:06:05 -0700

    Fixed comments

--- mix-list/mix-list.py
+++ mix-list/mix-list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 #Written by: Kyle Jorgensen
-#
+#This stores a list of mixed variables and reports them back.
 
+#Store variables here
 my_list = ['192.168.0.5', 5060, 'UP']
+
+#Now report them one at a time.
 print('The first item in the list (IP): ' + my_list[0])
 print('The second item in the list (port): ' + str(my_list[1]))
 print('The last item in the list (state): ' + my_list[2])
