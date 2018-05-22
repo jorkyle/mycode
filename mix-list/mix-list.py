22875ec62dfe316aafabe055519f502557908418
Kyle Jorgensen <quintillion977@gmail.com>
2018-05-22 09:04:46 -0700

    Added lab 8, mix-list.

---
+++ mix-list/mix-list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#Written by: Kyle Jorgensen
+#
+
+my_list = ['192.168.0.5', 5060, 'UP']
+print('The first item in the list (IP): ' + my_list[0])
+print('The second item in the list (port): ' + str(my_list[1]))
+print('The last item in the list (state): ' + my_list[2])
