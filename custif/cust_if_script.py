d8879d7d205bb882005505e5ba7099b7f594079f
Kyle Jorgensen <quintillion977@gmail.com>
2018-05-22 14:06:15 -0700

    Custom 'if', lab 13

---
+++ custif/cust_if_script.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#Written by: Kyle Jorgensen
+#This is an if test that asks where students work.
+
+##Ask user where they work
+employer = input('What company hired you as an apprentice? ')
+
+##Set up if statements:
+if employer.lower() == 'amazon':
+    print('Congratulations!  Amazon is a great place to work.')
+elif employer.lower() == 'comtech':
+    print('Well done! ComTech also pretty good.')
+else:
+    print(employer + ' is a garbage company! You\'re in the wrong room! Get out!')
+print('Exiting script.')
