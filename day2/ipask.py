1d6eec92bb8c7c4c0fab8c941b463d6bfafde3a0
Kyle Jorgensen <quintillion977@gmail.com>
2018-05-22 08:50:49 -0700

    Minor edits to ipask.py

--- day2/ipask.py
+++ day2/ipask.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #Written by: Kyle Jorgensen
-#This will ask the user their IP and MAC addresses and store them as variablesself.
+#Ask the user their IP and MAC addresses and store them as variables.
 
 #Ask for user input:
 ipadd = input('Tell me an IP address: ')
@@ -9,3 +9,4 @@ macadd = input('Tell me a a MAC address: ')
 
 #Report back the variables:
 print('You said your IP address is ', ipadd, ' and your MAC is ', macadd)
+print('Work complete!')
