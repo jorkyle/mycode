778ebcb8b85ee00dd60bd7b228452f4d40033ec9
Kyle Jorgensen <quintillion977@gmail.com>
2018-05-21 20:07:16 -0700

    adding files back

---
+++ lab_input/input_ip.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+#Written by: Kyle Jorgensen
+#Request user's IP address and report it back.
+
+##Collect user IP
+user_input = input('Please enter and IPv4 IP Address: ')
+
+##Repeat IP back to user
+print("You told me the IPv4 address is: " + user_input)
