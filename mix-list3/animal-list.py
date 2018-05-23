7327aa082211aa9eb12101cf23034943a0e6ae0f
Kyle Jorgensen <quintillion977@gmail.com>
2018-05-23 08:40:57 -0700

    Added printing of list.  Forgot yesterday.

--- mix-list3/animal-list.py
+++ mix-list3/animal-list.py
@@ -3,4 +3,12 @@
 #Written by: Kyle Jorgensen
 #This is just a simple animal list
 
+##Create the list:
 animals3 = ['dog', 'cat', 'kid', 'hen', 'cow', 'pig', 'bee', 'fly']
+
+##Report it back:
+for x in animals3:
+    print(x, end =' ')
+##Bid farewell!
+print() #New line inserted here
+print('That\'s all folks!')
