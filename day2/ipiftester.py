1447a2967809c33474e696126c230236da492b48
Kyle Jorgensen <quintillion977@gmail.com>
2018-05-23 10:09:29 -0700

    Cleaned up while loop.

--- day2/ipiftester.py
+++ day2/ipiftester.py
@@ -6,17 +6,16 @@
 iplist = [] #Define empty list
 donotuse = 'DO NOT USE THIS ADDRESS!!! (Omitted from list)' #Message for bad values.
 
+ipadd = input('Please enter an IP address or Q to quit: ') #Ask user for IP address
 ##Encase the whole script in while loop so you can run repeatedly and populate the list.
-while True:
-    ipadd = input('Please enter an IP address or Q to quit: ') #Ask user for IP address
-    if ipadd.upper() == 'Q':
-        print('Thanks for playing. Goodbye.')
-        break
-    elif ipadd == '10.10.3.1' or ipadd == '10.20.5.2': #The no-no addresses
+while ipadd.upper() != 'Q':
+    if ipadd == '10.10.3.1' or ipadd == '10.20.5.2': #The no-no addresses
         print(donotuse) #Blast the user with a warning message
         print('Your list remains: ', str(iplist))
-    elif ipadd == '':
+    elif ipadd == '': #In case the user provides no input
         print('You did not provide input')
     else:
         iplist.append(ipadd) #Append value to previously defined list
         print('IP added. Your list now contains the following: ' + str(iplist))
+    ipadd = input('Please enter another IP address or Q to quit: ') #Ask user for IP address
+print('Thanks for playing. Goodbye.') #Farewell message
