e4094dd87055d1027bca175912aa61c67930b88d
Kyle Jorgensen <quintillion977@gmail.com>
2018-05-24 14:20:13 -0700

    Lab 25.  Minor edits required to run.

---
+++ cfgread/cfg01.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+#Edited by: Kyle Jorgensen
+
+######## EXPLORE READ ##########
+## create file object in "r"ead mode
+
+configfile = open('vlanconfig.cfg', 'r')
+
+## display file to the screen - .read()
+print(configfile.read())
+
+## close file
+configfile.close()
+
+######## EXPLORE READLINES ##########
+## re-create file object to explore new method
+configfile = open('vlanconfig.cfg', 'r')
+
+## make a list of file lines - .readlines()
+configlist = configfile.readlines()
+print(configlist)
+
+## Iterate thrhough configlist
+for x in configlist:
+    print(x, end=' ')
+
+## Always close your file
+configfile.close()
